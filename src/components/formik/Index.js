0fc0c1673507ab29d7fe2f543ae8da785f3e2fb7
haybuka <paschal.nduleo@gmail.com>
2023-04-14 03:42:33 +0100

    Using formik, managing form states and handling validation

---
+++ src/components/formik/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import YoutubeForm from './YoutubeForm'
+
+const FormikIndex = () => {
+  return (
+    <div>
+        <YoutubeForm />
+    </div>
+  )
+}
+
+export default FormikIndex
---
+++ src/components/formik/YoutubeForm.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { useFormik } from "formik";
+const YoutubeForm = () => {
+  const initialValues = { name: "", email: "", channel: "" };
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  const validate = (values) => {
+    //must match values.name,values.email,values.channel
+    // error object must return errors.name,errors.email,errors.channel
+    //values should be a string
+    let errors = {};
+
+    if (!values.name) {
+      errors.name = "Required";
+    }
+
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!values.channel) {
+      errors.channel = "Required";
+    }
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+
+  const { handleChange, handleBlur, values, handleSubmit,errors,touched } = formik;
+    console.log(touched)
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleChange}
+            value={values.name}
+            onBlur={handleBlur}
+          />
+          {touched.name && errors.name && (<p className="error">{errors.name}</p>)}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            value={values.email}
+            onBlur={handleBlur}
+          />
+          {touched.email && errors.email && (<p className="error">{errors.email}</p>)}
+
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            onChange={handleChange}
+            value={values.channel}
+            onBlur={handleBlur}
+
+          />
+          {touched.channel && errors.channel && (<p className="error">{errors.channel}</p>)}
+
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default YoutubeForm;

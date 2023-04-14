83aa7a52b93e7e65a784c531890d8786c5d1b78a
haybuka <paschal.nduleo@gmail.com>
2023-04-14 05:33:18 +0100

    More on fields and render props patter in formik

--- src/components/formik/YoutubeForm.js
+++ src/components/formik/YoutubeForm.js
@@ -2,7 +2,13 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 const YoutubeForm = () => {
-  const initialValues = { name: "", email: "", channel: "" };
+  const initialValues = {
+    name: "",
+    email: "",
+    channel: "",
+    comments: "",
+    address: "",
+  };
 
   const onSubmit = (values) => {
     console.log(values);
@@ -12,6 +18,8 @@ const YoutubeForm = () => {
     name: Yup.string().required("Required"),
     email: Yup.string().email("Invalid emails format").required("Required"),
     channel: Yup.string().required("Required"),
+    comments: Yup.string().required("Required"),
+    address : Yup.string().required("Required"),
   });
 
   return (
@@ -37,6 +45,30 @@ const YoutubeForm = () => {
           <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel" />
           <ErrorMessage name="channel" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field type="text">
+            {(props) => {
+            //   console.log(props);
+              const { field, form, meta } = props;
+              console.log(meta)
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {/* {meta.touched && meta.error ? <p>{meta.error.address}</p> : null} */}
+                </div>
+              );
+            }}
+          </Field>
+          <ErrorMessage name="address" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" type="text" id="comments" name="comments" />
+          <ErrorMessage name="comments" />
         </div>
         <button type="submit">Submit</button>
       </Form>

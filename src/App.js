b7748e88ed637db9c09fd6c38957340619aad02a
haybuka <paschal.nduleo@gmail.com>
2023-04-19 04:23:33 +0100

    Basic table setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import "./App.css";
+import BasicTable from "./components/ReactTable/BasicTable";
 
 function App() {
   return (
     <div className="App">
-      <p>React Table</p>
+      <BasicTable />
     </div>
   );
 }
---
+++ src/components/ReactTable/BasicTable.js
@@ -0,0 +1,53 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import { COLUMNS } from "./columns";
+import MOCK_DATA from "../MOCK_DATA.json";
+import "./table.css";
+const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow }  = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <div>
+      {/* //pass in getTableProps */}
+      <table {...getTableProps()}>
+        {/* Header group goes into the head */}
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                //Header property coming from Header column in imported.
+                <th {...column.getHeaderProps()}> {column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        {/* //pass in getTableBodyProps */}
+
+        <tbody {...getTableBodyProps()}>
+          {/* row goes into the table body */}
+
+          {rows.map((row) => {
+            //in other to prepare the row
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BasicTable;

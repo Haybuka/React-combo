e8e5081b80b2462ddd84809fd49b9df754696418
haybuka <paschal.nduleo@gmail.com>
2023-04-19 03:40:52 +0100

    setup react table

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 // import logo from './logo.svg';
 import "./App.css";
-import DataFetching from "./components/DataFetching/DataFetching";
-import FormReducer from "./components/Hooks/FormReducer";
-import UseReducer from "./components/Hooks/useReducer";
-import ReducerWithContext from "./components/ReducerContext";
+
 function App() {
   return (
     <div className="App">
-    {/* <UseReducer /> */}
-    {/* <FormReducer /> */}
-    {/* <ReducerWithContext /> */}
-    <DataFetching />
+      <p>React Table</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ReducerContextProvider } from "./context/ReducerContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <ReducerContextProvider>
-      <App />
-    </ReducerContextProvider>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

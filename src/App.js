29f80cb9c33892fd2de771e989b605c698087435
haybuka <paschal.nduleo@gmail.com>
2023-09-13 22:41:28 +0100

    react redux starter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-// import logo from './logo.svg';
-// import "./App.css";
-import AllTableHooks from "./components/ReactTable/AllTableHooks";
-import BasicTable from "./components/ReactTable/BasicTable";
-import FilteringTable from "./components/ReactTable/FilteringTable";
-import PaginationTable from "./components/ReactTable/PaginationTable";
-import RowSelection from "./components/ReactTable/RowSelection";
-import SortingTable from "./components/ReactTable/SortingTable";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <BasicTable /> */}
-      {/* <SortingTable /> */}
-      {/* <FilteringTable /> */}
-      {/* <PaginationTable /> */}
-      {/* <RowSelection /> */}
-      <AllTableHooks />
+      <p>React Redux</p>
     </div>
   );
 }
